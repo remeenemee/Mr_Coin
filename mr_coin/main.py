6a289da381e5f0f0addcbc57be5793c2af9739b5
remeenemee <dk3880930@gmail.com>
2024-01-28 21:49:21 +0200

    edit: .gitignore 	new file: mr_coin/main.py 	edit: poetry.lock 	edit: pyproject.toml

---
+++ mr_coin/main.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+from id_token import token
+
+
+API_TOKEN = token
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {hbold(message.from_user.full_name)}!\nЯ Mr.Coin 🤓!\nЯ буду твоим финансовым бухгалтером\nи помогу тебе вести анализ доходов и расходов")
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(API_TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
